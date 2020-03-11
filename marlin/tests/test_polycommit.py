c8a3583fa810b880120b7653a3878dea027e7978
sanket1729 <sanket1729@gmail.com>
2020-03-10 22:35:36 -0500

    Polycommit tests added

---
+++ marlin/tests/test_polycommit.py
@@ -0,0 +1,15 @@
+from ssbls12 import Fp, Poly, Group
+from polycommit import PolyCommit
+import sys, os
+
+sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/../.."))
+sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/.."))
+
+def test_polycommit():
+	f = Poly([Fp(1), Fp(4), Fp(2), Fp(3)])
+	pc = PolyCommit(max_degree=5)
+	C = pc.commit(f)
+	i = Fp(10)
+	proof = pc.create_witness(f, i)
+
+	assert pc.verify_eval(C, proof)
